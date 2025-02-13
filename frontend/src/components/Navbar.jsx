e6ab310d47064276e6e1c0b323ed47b1c3ec19ae
Azhagu <azhagukarthikeyanb@gmail.com>
2025-02-13 12:30:47 +0530

    Added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,14 +3,15 @@ import {FaBars} from 'react-icons/fa'
 import { useContext, useState } from "react"
 import Menu from "./Menu"
 import { UserContext } from "../context/UserContext"
+import {BsSearch} from 'react-icons/bs'
 
 
 
 const Navbar = () => {
   
-  // const [prompt,setPrompt]=useState("")
+  const [prompt,setPrompt]=useState("")
   const [menu,setMenu]=useState(false)
-  // const navigate=useNavigate()
+  const navigate=useNavigate()
   const path=useLocation().pathname
   
   // console.log(prompt)
@@ -27,8 +28,8 @@ const Navbar = () => {
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
     <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Blogify</Link></h1>
     {path==="/" && <div className="flex justify-center items-center space-x-0">
-    {/* <p onClick={()=>navigate(prompt?"?search="+prompt:navigate("/"))} className="cursor-pointer"><BsSearch/></p> */}
-    {/* <input onChange={(e)=>setPrompt(e.target.value)} className="outline-none px-3 " placeholder="Search a post" type="text"/> */}
+    <p onClick={()=>navigate(prompt?"?search="+prompt:navigate("/"))} className="cursor-pointer"><BsSearch/></p>
+    <input onChange={(e)=>setPrompt(e.target.value)} className="outline-none px-3 " placeholder="Search a post" type="text"/>
     
     </div>}
     <div className="hidden md:flex items-center justify-center space-x-2 md:space-x-4">
